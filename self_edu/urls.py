61ec7711991c2b92db4ca1a0b98dcc4ab5670701
ro_k_ <ro_k_sana@mail.ru>
2024-08-28 05:43:49 +0500

    исправления

--- self_edu/urls.py
+++ self_edu/urls.py
@@ -44,8 +44,8 @@ urlpatterns = [
     # документирование средствами drf-yasg
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # документирование средствами drf-spectacular
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 
